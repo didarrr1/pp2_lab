b6d288ff4ed5d7532a7708e458a49cf87eae1539
didarrr1 <bakirdidar2@gmail.com>
2025-01-21 23:03:15 +0500

    lab1

--- PythonStrings.py
+++ PythonStrings.py
@@ -142,47 +142,48 @@ print(txt)
 
 a = "Hello, World! {}"
 
-print(a.capitalize())
-print(a.casefold())
-print(a.center(20))
-print(a.count("e"))
-print(a.encode())
-print(a.endswith("!"))
-print(a.expandtabs(2))
-print(a.find("e"))
-print(a.format("John"))
-print(a.index("e"))
-print(a.isalnum())
-print(a.isalpha())
-print(a.isascii())
-print(a.isdecimal())
-print(a.isdigit())
-print(a.isidentifier())
-print(a.islower())
-print(a.isnumeric())
-print(a.isprintable())
-print(a.isspace())
-print(a.istitle())
-print(a.isupper())
-print(a.join("John"))
-print(a.ljust(20))
-print(a.lower())
-print(a.lstrip())
-print(a.maketrans("H", "J"))
-print(a.partition("e"))
-print(a.replace("e", "a"))
-print(a.rfind("e"))
-print(a.rindex("e"))
-print(a.rjust(20))
-print(a.rpartition("e"))
-print(a.rsplit())
-print(a.rstrip())
-print(a.split())
-print(a.splitlines())
-print(a.startswith("H"))
-print(a.strip())
-print(a.swapcase())
-print(a.title())
-print(a.translate("H"))
-print(a.upper())
-print(a.zfill(5))
+capitalize()	Converts the first character to upper case
+casefold()	Converts string into lower case
+center()	Returns a centered string
+count()	Returns the number of times a specified value occurs in a string
+encode()	Returns an encoded version of the string
+endswith()	Returns true if the string ends with the specified value
+expandtabs()	Sets the tab size of the string
+find()	Searches the string for a specified value and returns the position of where it was found
+format()	Formats specified values in a string
+format_map()	Formats specified values in a string
+index()	Searches the string for a specified value and returns the position of where it was found
+isalnum()	Returns True if all characters in the string are alphanumeric
+isalpha()	Returns True if all characters in the string are in the alphabet
+isascii()	Returns True if all characters in the string are ascii characters
+isdecimal()	Returns True if all characters in the string are decimals
+isdigit()	Returns True if all characters in the string are digits
+isidentifier()	Returns True if the string is an identifier
+islower()	Returns True if all characters in the string are lower case
+isnumeric()	Returns True if all characters in the string are numeric
+isprintable()	Returns True if all characters in the string are printable
+isspace()	Returns True if all characters in the string are whitespaces
+istitle()	Returns True if the string follows the rules of a title
+isupper()	Returns True if all characters in the string are upper case
+join()	Joins the elements of an iterable to the end of the string
+ljust()	Returns a left justified version of the string
+lower()	Converts a string into lower case
+lstrip()	Returns a left trim version of the string
+maketrans()	Returns a translation table to be used in translations
+partition()	Returns a tuple where the string is parted into three parts
+replace()	Returns a string where a specified value is replaced with a specified value
+rfind()	Searches the string for a specified value and returns the last position of where it was found
+rindex()	Searches the string for a specified value and returns the last position of where it was found
+rjust()	Returns a right justified version of the string
+rpartition()	Returns a tuple where the string is parted into three parts
+rsplit()	Splits the string at the specified separator, and returns a list
+rstrip()	Returns a right trim version of the string
+split()	Splits the string at the specified separator, and returns a list
+splitlines()	Splits the string at line breaks and returns a list
+startswith()	Returns true if the string starts with the specified value
+strip()	Returns a trimmed version of the string
+swapcase()	Swaps cases, lower case becomes upper case and vice versa
+title()	Converts the first character of each word to upper case
+translate()	Returns a translated string
+upper()	Converts a string into upper case
+zfill()	Fills the string with a specified number of 0 values at the beginning
